3f395d80c748f6ec770dd27b7e7570f47aebcc95
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-06-16 23:35:09 +0000

    Added all model

---
+++ backend/model/database.py
@@ -0,0 +1,40 @@
+import uuid
+from datetime import datetime
+
+
+class Database:
+    """
+    Database class represent the database configuration.
+    """
+
+    __collection_name = "database"
+
+    @classmethod
+    def collection_name(cls):
+        return cls.__collection_name
+
+    def __int__(
+        self,
+        user_id: str,
+        db_type: str,
+        host: str,
+        port: str,
+        username: str,
+        password: str,
+        database_name: str,
+    ):
+        """
+        Initializes a new Database instance.
+        """
+
+        self._id = str(uuid.uuid4())
+        self.user_id = user_id
+        self.db_type = db_type
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database_name = database_name
+        self.password = password
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
---
+++ backend/storage/db.py
@@ -0,0 +1,39 @@
+import os
+from pymongo import MongoClient
+
+
+class MongoDBConnection:
+    """
+    MongoDBConnection class manages the connection to a MongoDB database.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new MongoDBConnection instance, connecting to the MongoDB database.
+        """
+        # Get connection host and port
+        db_name = os.environ.get("MONGO_DB_NAME")
+        connection_string = os.environ.get("MONGO_DB_URI")
+
+        if not db_name or not connection_string:
+            raise ValueError(
+                "Both MONGO_DB_NAME and MONGO_DB_URI environment variables must be set"
+            )
+
+        try:
+            # Connect to database
+            self.client = MongoClient(connection_string)
+            self.db = self.client[db_name]
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+            raise
+
+
+if __name__ == "__main__":
+    try:
+        connection = MongoDBConnection()
+        db = connection.db
+        client = connection.client
+        print("Connected to the database successfully")
+    except Exception as e:
+        print(f"Failed to connect to the database: {e}")
