19c6c341f7bb96356218dbda9230538d2bc041c4
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-06-17 23:09:14 +0000

    Fix: mail port None value

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,29 +5,17 @@ from flask_mail import Mail
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from .storage.db import MongoDBConnection
-from .storage.constant import UPLOAD_FOLDER
 
-
+# Create app
 app = Flask(__name__)
-
-# configure the file upload folder
-# and application security setting
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
-app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
-app.config["MAIL_PORT"] = int(os.environ.get("MAIL_PORT"))
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-app.config["MAIL_USE_TLS"] = True if os.environ.get("MAIL_USE_TLS") == "true" else False
-app.config["MAIL_USE_SSL"] = True if os.environ.get("MAIL_USE_SSL") == "true" else False
-
 jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 mail = Mail(app)
+
 # CORS(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+# Connect to database
 try:
     connection = MongoDBConnection()
     mongo_db = connection.db
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
+import os
 from flask import jsonify
 from dotenv import find_dotenv, load_dotenv
-
+from .storage.constant import UPLOAD_FOLDER
 # Load enviroment variable
 load_dotenv(find_dotenv())
 
@@ -11,6 +12,18 @@ from .database_config.main import db_config_app
 from .backup_agent.main import backup_agent_app
 from .backup_management.main import backup_management_app
 
+# configure the file upload folder
+# and application security setting
+print('os.environ.get("MAIL_PORT: ")', os.environ.get("MAIL_PORT"))
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = int(os.environ.get("MAIL_PORT"))
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_USE_TLS"] = True if os.environ.get("MAIL_USE_TLS") == "true" else False
+app.config["MAIL_USE_SSL"] = True if os.environ.get("MAIL_USE_SSL") == "true" else False
 
 app.register_blueprint(user_app)
 app.register_blueprint(schedule_app)
