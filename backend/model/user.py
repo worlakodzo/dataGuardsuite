d12e03d8dfbb33c3e72da05adcd373df71131b39
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-06-18 01:21:07 +0000

    Feat: implemented user exists functionality

--- backend/model/user.py
+++ backend/model/user.py
@@ -61,6 +61,7 @@ class User:
             The result of the insert operation.
         """
         if not hasattr(self, "update_document"):
+            # Insert a new document
             res = mongo_db.user.insert_one(self.__dict__)
         else:
             # Update existing document
@@ -79,3 +80,65 @@ class User:
         )
         msg.body = f"Your password recovery token is: {recovery_token}"
         mail.send(msg)
+
+    @staticmethod
+    def email_exists(email:str) -> bool:
+        """
+        Filters documents from a MongoDB collection based on email.
+        Returns:
+            True or False
+        """
+        # Retrieve documents matching the query
+        res = mongo_db.user.find_one({"email": email})
+        if res:
+            return True
+        return False
+    
+    @staticmethod
+    def username_exists(username:str) -> bool:
+        """
+        Filters documents from a MongoDB collection based on username.
+        Returns:
+            True or False
+        """
+        # Retrieve documents matching the query
+        res = mongo_db.user.find_one({"username": username})
+        if res:
+            return True
+        return False
+
+    @staticmethod
+    def filter(query: dict, projection=None) -> list:
+        """
+        Filters documents from a MongoDB collection based on a query.
+
+        Args:
+            query (dict): The query to filter documents.
+            projection (dict, optional): The fields to include or exclude. Defaults to None.
+        Returns:
+            list: A list of filtered documents converted to user instance.
+        """
+        # Retrieve documents matching the query
+        res = mongo_db.user.find(query, projection)
+        
+        objects = []
+        for _, document in enumerate(res):
+            new_obj = User()
+            for key, value in document.items():
+                setattr(new_obj, key, value)
+
+            setattr(new_obj, "update_document", True)
+            objects.append(new_obj)
+
+        return objects
+    
+    def to_dict(self):
+        """
+        Convert user instance to dictionary.
+        """
+        data_dict = self.__dict__
+        data_dict["created_at"] = self.created_at.isoformat()
+        data_dict["updated_at"] = self.updated_at.isoformat()
+        data_dict["user_profile"]["created_at"] = self.user_profile["created_at"].isoformat()
+        data_dict["user_profile"]["updated_at"] = self.user_profile["updated_at"].isoformat()
+        return data_dict
